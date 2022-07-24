bd0d61de8f1c22ec15fcd59e1688761ae8b503e4
RedSuns Chan <redsunschan@gmail.com>
2022-07-24 23:17:59 +0800

    + sfx file extension name convertor

---
+++ sdgo-sfx-extractor.py
@@ -0,0 +1,22 @@
+# sdgo-sfx-extractor
+# author: RedSuns Chan
+# rename file extension to .wav
+
+import os
+from pathlib import Path
+import shutil
+
+sdgo_data_path = "D:/Project/SDGO/data/"
+target_path = sdgo_data_path + "sdrs/decoded/"
+
+def main():
+	print("sdgo sfx extractor")
+	Path(target_path).mkdir(parents=True, exist_ok=True)
+	file_list = os.listdir(sdgo_data_path + "sdrs")
+	for file_name in file_list:
+		if file_name.endswith(".zsd"):
+			file_name = file_name.replace(".zsd", "")
+			print("converting " + file_name + ".zsd >>>>> " + file_name + ".wav")
+			shutil.copyfile(sdgo_data_path + "sdrs/" + file_name + ".zsd", target_path + file_name + ".wav")
+
+main()
