58e89ba0793843d0523776002afc21ef111f4773
RedSuns Chan <redsunschan@gmail.com>
2022-07-25 10:17:06 +0800

    + the start file of blender plugin

---
+++ blender-hex-to-obj.py
@@ -0,0 +1,14 @@
+
+import bpy
+
+bl_info = {
+    "name": "hex-to-obj blender",
+    "blender": (3, 2, 1),
+    "category": "Object",
+}
+
+def register():
+    print("register hex-to-obj")
+
+def unregister():
+    print("de-register hex-to-obj")
